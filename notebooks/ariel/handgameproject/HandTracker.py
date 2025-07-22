1b4aa7dbbf068443df01680c8176852cb0f317f4
Cosmicflam <arielyartsev@gmail.com>
2025-07-22 08:28:54 -0700

    created something that can graph the data, and something that can collect the data

--- notebooks/ariel/handgameproject/HandTracker.py
+++ notebooks/ariel/handgameproject/HandTracker.py
@@ -31,7 +31,7 @@ class HandTracker:
         
         return self.index_tip_pos
 
-    def wristdist(self):
+    def wristdist(self): #index top to wrist
         success, frame = self.cap.read()
         if not success:
             return None
@@ -67,7 +67,7 @@ class HandTracker:
             elif distance > 0:
                 return distance
             
-    def pinkywrist(self):
+    def pinkywrist(self): #pinky top to wrist
         success, frame = self.cap.read()
         if not success:
             return None
@@ -104,7 +104,7 @@ class HandTracker:
                 return distance
 
         return None
-    def thumbwrist(self):
+    def thumbwrist(self): #thumb top to wrist
         success, frame = self.cap.read()
         if not success:
             return None
@@ -128,7 +128,6 @@ class HandTracker:
             hand_height = ((x2 - x3) ** 2 + (y2 - y3) ** 2) ** 0.5
             hand_size = (hand_width + hand_height) / 2
 
-            # Now calculate distance from wrist to index tip
             tip_x = int(thumbtop.x * self.width)
             tip_y = int(thumbtop.y * self.height)
             wrist_x = int(wrist.x * self.width)
@@ -143,7 +142,7 @@ class HandTracker:
         return None
 
 
-    def indexwrist(self):
+    def indexwrist(self): 
         success, frame = self.cap.read()
         if not success:
             return None
@@ -167,7 +166,6 @@ class HandTracker:
             hand_height = ((x2 - x3) ** 2 + (y2 - y3) ** 2) ** 0.5
             hand_size = (hand_width + hand_height) / 2
 
-            # Now calculate distance from wrist to index tip
             tip_x = int(indextop.x * self.width)
             tip_y = int(indextop.y * self.height)
             wrist_x = int(wrist.x * self.width)
@@ -178,5 +176,93 @@ class HandTracker:
                 return (distance / hand_size)
             elif distance > 0:
                 return distance
+    def ringwrist(self): 
+        success, frame = self.cap.read()
+        if not success:
+            return None
+        
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(frame_rgb)
+        
+        if results.multi_hand_landmarks:
+            hand = results.multi_hand_landmarks[0]
+            pinkymcp = hand.landmark[17]
+            indexmcp = hand.landmark[5]
+            wrist = hand.landmark[0]
+            middlemcp = hand.landmark[9]
+            ringtop = hand.landmark[16]
 
+            x1, y1 = int(pinkymcp.x * self.width), int(pinkymcp.y * self.height)
+            x2, y2 = int(wrist.x * self.width), int(wrist.y * self.height)
+            x3, y3 = int(indexmcp.x * self.width), int(indexmcp.y * self.height)
+
+            hand_width = ((x1 - x3) ** 2 + (y1 - y3) ** 2) ** 0.5
+            hand_height = ((x2 - x3) ** 2 + (y2 - y3) ** 2) ** 0.5
+            hand_size = (hand_width + hand_height) / 2
+
+            
+            tip_x = int(ringtop.x * self.width)
+            tip_y = int(ringtop.y * self.height)
+            wrist_x = int(wrist.x * self.width)
+            wrist_y = int(wrist.y * self.height)
+
+            distance = ((tip_x - wrist_x) ** 2 + (tip_y - wrist_y) ** 2) ** 0.5
+            if hand_size > 0:
+                return (distance / hand_size)
+            elif distance > 0:
+                return distance
+        return None
+    def handsize(self, frame, results):
+        if results.multi_hand_landmarks:
+            hand = results.multi_hand_landmarks[0]
+
+            h, w, _ = frame.shape  
+
+            pinky = hand.landmark[17]
+            index = hand.landmark[5]
+            wrist = hand.landmark[0]
+
+            x1, y1 = pinky.x * w, pinky.y * h
+            x2, y2 = wrist.x * w, wrist.y * h
+            x3, y3 = index.x * w, index.y * h
+
+            hand_width = ((x1 - x3)**2 + (y1 - y3)**2)**0.5
+            hand_height = ((x2 - x3)**2 + (y2 - y3)**2)**0.5
+            hand_size = (hand_width + hand_height) / 2
+
+            return hand_size
+    def middlewrist(self):
+        success, frame = self.cap.read()
+        if not success:
+            return None
+        
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(frame_rgb)
+        
+        if results.multi_hand_landmarks:
+            hand = results.multi_hand_landmarks[0]
+            pinkymcp = hand.landmark[17]
+            indexmcp = hand.landmark[5]
+            wrist = hand.landmark[0]
+            middlemcp = hand.landmark[9]
+            pinkytop = hand.landmark[20]
+
+            x1, y1 = int(pinkymcp.x * self.width), int(pinkymcp.y * self.height)
+            x2, y2 = int(wrist.x * self.width), int(wrist.y * self.height)
+            x3, y3 = int(indexmcp.x * self.width), int(indexmcp.y * self.height)
+
+            hand_width = ((x1 - x3) ** 2 + (y1 - y3) ** 2) ** 0.5
+            hand_height = ((x2 - x3) ** 2 + (y2 - y3) ** 2) ** 0.5
+            hand_size = (hand_width + hand_height) / 2
+
+            tip_x = int(middlemcp.x * self.width)
+            tip_y = int(middlemcp.y * self.height)
+            wrist_x = int(wrist.x * self.width)
+            wrist_y = int(wrist.y * self.height)
+
+            distance = ((tip_x - wrist_x) ** 2 + (tip_y - wrist_y) ** 2) ** 0.5
+            if hand_size > 0:
+                return (distance / hand_size)
+            elif distance > 0:
+                return distance
         return None
